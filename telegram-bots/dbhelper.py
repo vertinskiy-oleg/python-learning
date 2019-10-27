8f78107fc7a6aa171163cd57a2359065372d997f
Oleg <decruel89@gmail.com>
2019-10-27 11:45:07 +0200

    tg-bots-learning

--- telegram-bots/dbhelper.py
+++ telegram-bots/dbhelper.py
@@ -1,1 +1,35 @@
 import sqlite3
+
+class DBHelper():
+    def __init__(self, name="todo.sqlite"):
+        self.name = name
+        self.conn = sqlite3.connect(name)
+
+    def create(self):
+        query = 'CREATE TABLE IF NOT EXISTS items (user text, description text)'
+        user_index = 'CREATE INDEX IF NOT EXISTS userIndex ON items (user ASC)'
+        item_index = 'CREATE INDEX IF NOT EXISTS itemIndex ON items (description ASC)'
+        self.conn.execute(query)
+        self.conn.execute(user_index)
+        self.conn.execute(item_index)
+        self.conn.commit()
+    
+    def add_item(self, user, item):
+        query = 'INSERT INTO items (user, description) VALUES (?,?)'
+        args = (user, item)
+        self.conn.execute(query, args)
+        self.conn.commit()
+
+    def delete_item(self, user, item):
+        query = 'DELETE FROM items WHERE user = (?) AND description = (?)'
+        args = (user, item)
+        self.conn.execute(query, args)
+        self.conn.commit()
+
+    def get_items(self, user):
+        query = 'SELECT description FROM items WHERE user = (?)'
+        args = (user, )
+        items = self.conn.execute(query, args)
+        return [item[0] for item in items]
+
+    
--- telegram-bots/todobot.py
+++ telegram-bots/todobot.py
@@ -1,9 +1,13 @@
 import time
 import requests
+import json
+from dbhelper import DBHelper
 
 TOKEN = '734516183:AAEs81N8bZdFK4YqwpBsYm7CODXNoe-1nqY'
 
 URL = 'https://api.telegram.org/bot{}/'.format(TOKEN)
+
+db = DBHelper()
 
 def get_updates(offset=None):
     url = URL + 'getUpdates'
@@ -12,34 +16,63 @@ def get_updates(offset=None):
         'offset': offset
     }
     res = requests.get(url, params=params)
-    data = res.json()
-    return data
+    updates = res.json()
+    return updates
 
-def get_last_update_id(data):
-    return data['result'][-1]['update_id']
+def get_last_update_id(updates):
+    return updates['result'][-1]['update_id']
 
-def send_message(chat_id, text):
+def send_message(chat_id, message, reply_markup=None):
     url = URL + 'sendMessage'
     params = {
     'chat_id': chat_id,
-    'text': text 
+    'text': message 
     }
+    if reply_markup:
+        params['reply_markup'] = reply_markup
     requests.get(url, params)
 
-def echo_all(data):
-    for d in data["result"]:
-        text = d["message"]["text"]
-        chat_id = d["message"]["chat"]["id"]
-        send_message(chat_id, text)
+def build_keyboard(items):
+    keyboard = [[item] for item in items]
+    reply_markup = {"keyboard":keyboard, "one_time_keyboard": True}
+    return json.dumps(reply_markup)
+
+def handle_updates(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat_id = update["message"]["chat"]["id"]
+            items = db.get_items(chat_id)
+            if text == '/done':
+                keyboard = build_keyboard(items)
+                send_message(chat_id, 'Select an item to delete', keyboard)
+            elif text == "/start":
+                send_message(chat_id, '''Welcome to your personal To Do list. Send any text to me and 
+                                I'll store it as an item. Send /done to remove items''')
+            elif text.startswith("/"):
+                continue
+            elif text in items:
+                db.delete_item(chat_id, text)
+                items = db.get_items(chat_id)
+                keyboard = build_keyboard(items)
+                send_message(chat_id, 'Select an item to delete', keyboard)
+            else:
+                db.add_item(chat_id, text)
+                items = db.get_items(chat_id)
+            message = "\n".join(items)
+            send_message(chat_id, message)
+        except KeyError:
+            pass
 
 def main():
+    db.create()
     last_update_id = None
     while True:
         print('updating')
         updates = get_updates(last_update_id)
         if len(updates["result"]) > 0:
             last_update_id = get_last_update_id(updates) + 1
-            echo_all(updates)
+            handle_updates(updates)
         time.sleep(0.5)
 
 if __name__ == '__main__':
